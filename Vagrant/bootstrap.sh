3f92fe24b1b645da3d865df0bb99e021545bf9a3
tilenn <to8803@student.uni-lj.si>
2021-12-13 19:49:28 +0100

    first working version

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-# gns3
-# echo "installing gns3-server, gns3-gui"
-# sudo add-apt-repository ppa:gns3/ppa -y
-# sudo apt update -y
-# sudo apt install -y gns3-server
-# sudo apt install -y gns3-gui
-
 echo "updating..."
 sudo apt update -y
 
 echo "installing desktop environment..."
 sudo apt install xorg -y
 sudo apt install lxde -y
+
+echo "installing gns3-server, gns3-gui"
+sudo add-apt-repository ppa:gns3/ppa -y
+sudo apt update -y
+sudo apt install -y gns3-server
+sudo apt install -y gns3-gui
 
 echo "rebooting"
 sudo reboot
