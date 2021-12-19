127f8b9a8d4d76a27b216ee2df1c2628ce5f7b81
tilenn <to8803@student.uni-lj.si>
2021-12-19 02:05:23 +0100

    added firefox

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -4,15 +4,18 @@ export DEBIAN_FRONTEND=noninteractive
 echo "updating..."
 sudo apt update -y
 
-echo "installing desktop environment..."
+echo "installing lightweight desktop environment..."
 sudo apt install xorg -y
 sudo apt install lxde -y
 
-echo "installing gns3-server, gns3-gui"
+echo "installing gns3-server, gns3-gui..."
 sudo add-apt-repository ppa:gns3/ppa -y
 sudo apt update -y
 sudo apt install -y gns3-server
 sudo apt install -y gns3-gui
 
-echo "rebooting"
+echo "installing firefox..."
+sudo apt install firefox -y
+
+echo "rebooting..."
 sudo reboot
